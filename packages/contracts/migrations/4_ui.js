1edb40acb0ad5cc7ae975afe753bb77e9bfaa08c
Wanseob Lim <email@wanseob.com>
2020-09-14 22:16:59 +0900

    refactor: delete unused mimc deployment; closes #64

--- packages/contracts/migrations/4_ui.js
+++ packages/contracts/migrations/4_ui.js
@@ -1,9 +1,7 @@
 const Poseidon6 = artifacts.require('Poseidon6')
-const MiMC = artifacts.require('MiMC')
 const UserInteractable = artifacts.require('UserInteractable')
 
 module.exports = function migration(deployer) {
   deployer.link(Poseidon6, UserInteractable)
-  deployer.link(MiMC, UserInteractable)
   deployer.deploy(UserInteractable)
 }
