9a7a5b8334087c381e3d9132620414f90fda0e6e
Wanseob Lim <email@wanseob.com>
2020-09-14 20:07:10 +0900

    fix: add spdx license comments

--- packages/contracts/contracts/controllers/UserInteractable.sol
+++ packages/contracts/contracts/controllers/UserInteractable.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity = 0.6.12;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
--- packages/contracts/contracts/libraries/Hash.sol
+++ packages/contracts/contracts/libraries/Hash.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity = 0.6.12;
 
 import { Hasher } from "./Tree.sol";
