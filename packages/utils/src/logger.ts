b7161850b184152fb28acfee62bdfed38cf9996f
Chance Hudson <jchancehud@gmail.com>
2021-02-04 23:25:52 -0600

    fix: enable pretty logs

--- packages/utils/src/logger.ts
+++ packages/utils/src/logger.ts
@@ -34,7 +34,10 @@ export class StreamConcatenator extends Writable {
 }
 
 export const logStream = new StreamConcatenator()
-export const logger = pino({ level: 'info' }, logStream)
+export const logger = pino({
+  level: 'info',
+  prettyPrint: true,
+}, logStream)
 
 const pinoPrettier = prettier({
   translateTime: false,
