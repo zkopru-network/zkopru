f5832999c914997be2ffe50767da625be2a01275
Chance Hudson <jchancehud@gmail.com>
2021-02-17 00:43:52 -0600

    fix: linter battle

--- packages/client/tests/index.test.ts
+++ packages/client/tests/index.test.ts
@@ -1,12 +1,14 @@
 import assert from 'assert'
 import Zkopru from '../src'
 
-test('should use node-fetch implementation', async () => {
-  const zkopru = new Zkopru('http://localhost')
-  try {
-    await zkopru.rpc.getAddress()
-    assert(false)
-  } catch (err) {
-    assert(true)
-  }
-}, 10000)
+describe('client tests', () => {
+  it('should use node-fetch implementation', async () => {
+    const zkopru = new Zkopru('http://localhost')
+    try {
+      await zkopru.rpc.getAddress()
+      assert(false)
+    } catch (err) {
+      assert(true)
+    }
+  }, 10000)
+})
