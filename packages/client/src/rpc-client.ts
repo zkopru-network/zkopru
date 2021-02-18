d86a99c56785622850db95e55c1076d1f81ed0b9
Chance Hudson <jchancehud@gmail.com>
2021-02-17 23:13:22 -0600

    feat: add new methods to client, enumerate rpc methods

--- packages/client/src/rpc-client.ts
+++ packages/client/src/rpc-client.ts
@@ -1,3 +1,4 @@
+import { RpcMethod } from '@zkopru/coordinator'
 import { RpcType, RpcConfig, Block, Tx, Registry } from './types'
 import fetch from './fetch'
 
@@ -9,36 +10,58 @@ export default class RpcClient {
   }
 
   async getAddress(): Promise<string> {
-    const { result } = await this.callMethod('l1_address')
+    const { result } = await this.callMethod(RpcMethod.address)
     return result
   }
 
-  async getBlockNumber(): Promise<number> {
-    const { result } = await this.callMethod('l2_blockNumber')
+  async syncing(): Promise<boolean> {
+    const { result } = await this.callMethod(RpcMethod.syncing)
+    return result
+  }
+
+  async getProposalCount(): Promise<number> {
+    const { result } = await this.callMethod(RpcMethod.blockCount)
     return +result
   }
 
-  async getBlockByNumber(num: number | string): Promise<Block> {
-    const { result } = await this.callMethod('l2_getBlockByNumber', num)
+  async getBlockNumber(): Promise<number> {
+    const { result } = await this.callMethod(RpcMethod.blockNumber)
+    return +result
+  }
+
+  async getBlockByIndex(num: number | 'latest'): Promise<Block> {
+    const { result } = await this.callMethod(RpcMethod.blockByIndex, num)
     return result
   }
 
-  async getBlockByHash(hash: string): Promise<Block> {
-    const { result } = await this.callMethod('l2_getBlockByHash', hash)
+  async getBlockByNumber(
+    num: number | 'latest',
+    includeUncles = false,
+  ): Promise<Block> {
+    const { result } = await this.callMethod(
+      RpcMethod.blockByNumber,
+      num,
+      includeUncles,
+    )
+    return result
+  }
+
+  async getBlockByHash(hash: string | 'latest'): Promise<Block> {
+    const { result } = await this.callMethod(RpcMethod.blockByHash, hash)
     return result
   }
 
   async getTransactionByHash(hash: string): Promise<Tx> {
-    const { result } = await this.callMethod('l2_getTransactionByHash', hash)
+    const { result } = await this.callMethod(RpcMethod.transactionByHash, hash)
     return result
   }
 
   async getRegisteredTokens(): Promise<Registry> {
-    const { result } = await this.callMethod('l2_getRegisteredTokens')
+    const { result } = await this.callMethod(RpcMethod.registeredTokens)
     return result
   }
 
-  private async callMethod(method: string, ...params: any[]) {
+  private async callMethod(method: RpcMethod, ...params: any[]) {
     if (this.config.type === RpcType.http) {
       const response = await fetch(this.config.url, {
         method: 'POST',
