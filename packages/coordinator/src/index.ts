8f14f50a4e3100241f20ce55bdb12e901906a5d4
Chance Hudson <jchancehud@gmail.com>
2021-02-17 23:13:22 -0600

    feat: add new methods to client, enumerate rpc methods

--- packages/coordinator/src/index.ts
+++ packages/coordinator/src/index.ts
@@ -1,3 +1,4 @@
 // export { Coordinator, CoordinatorInterface } from './coordinator'
 export { TxMemPool, TxPoolInterface } from './tx-pool'
+export { RpcMethod } from './client-api'
 export { Coordinator } from './coordinator'
