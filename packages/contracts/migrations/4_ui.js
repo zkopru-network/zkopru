4265a39a7083122c76e1a275fe67bf84733b010d
Chance Hudson <jchancehud@gmail.com>
2021-03-25 00:19:01 -0500

    fix: ensure poseidon contracts exist for linking

--- packages/contracts/migrations/4_ui.js
+++ packages/contracts/migrations/4_ui.js
@@ -4,6 +4,8 @@ const UserInteractable = artifacts.require("UserInteractable");
 
 module.exports = function migration(deployer) {
   return deployer.then(async () => {
+    await deployer.deploy(Poseidon3, { overwrite: false });
+    await deployer.deploy(Poseidon4, { overwrite: false });
     await deployer.link(Poseidon3, UserInteractable);
     await deployer.link(Poseidon4, UserInteractable);
     await deployer.deploy(UserInteractable);
--- packages/contracts/migrations/6_utxo_tree_validator.js
+++ packages/contracts/migrations/6_utxo_tree_validator.js
@@ -3,6 +3,7 @@ const UtxoTreeValidator = artifacts.require("UtxoTreeValidator");
 
 module.exports = function migration(deployer) {
   deployer.then(async () => {
+    await deployer.deploy(Poseidon2, { overwrite: false });
     await deployer.link(Poseidon2, UtxoTreeValidator);
     await deployer.deploy(UtxoTreeValidator);
   });
