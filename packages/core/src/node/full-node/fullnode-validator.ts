5a642168b8e0618726a55592d27c8146ff31092c
Chance Hudson <jchancehud@gmail.com>
2021-03-28 23:12:28 -0500

    fix: use logger instead of console

--- packages/core/src/node/full-node/fullnode-validator.ts
+++ packages/core/src/node/full-node/fullnode-validator.ts
@@ -77,8 +77,8 @@ export class FullValidator extends Validator {
     ) {
       onchainResult.forEach((result, index) => {
         if (result.slashable !== offchainResult[index].slashable) {
-          console.log('onchain', onchainResult[index])
-          console.log('offchain', offchainResult[index])
+          logger.info(`onchain slashable: ${(onchainResult[index] as any).slashable}`)
+          logger.info(`offchain slashable: ${(offchainResult[index] as any).slashable}`)
         }
       })
       logger.error(
