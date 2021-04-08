77a5c6c0cdeaf1c36f35919c00fb9d479c9cdd3e
Jin <sifnoc09@gmail.com>
2021-04-09 01:01:29 +0900

    fixed ready check script

--- scripts/wait_deploy_contracts.sh
+++ scripts/wait_deploy_contracts.sh
@@ -2,9 +2,8 @@
 target_contract=$(cat /proj/packages/cli/coordinator.dev.json | jq .address)
 
 function get_code_length() {
-  response_length=$(curl -s -H "Content-Type: application/json" -X POST --data '{"id":1337,"jsonrpc":"2.0","method":"eth_getCode","params":['$target_contract']}' http://$target_host:5000 | jq .result | wc -m)
+  response_length=$(curl -s -H "Content-Type: application/json" -X POST --data '{"id":1337,"jsonrpc":"2.0","method":"eth_getCode","params":['$target_contract']}' http://$TARGET_HOST:5000 | jq .result | wc -m)
 }
-
 
 while true; do
   get_code_length
@@ -14,5 +13,5 @@ while true; do
   else
     echo "Wait for finish deploying Zkopru contract"
     sleep 1
- fi  
+  fi
 done
