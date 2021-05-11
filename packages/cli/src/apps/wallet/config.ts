7678d1a2953e1783a2962038f0bd97a54a380789
Chance Hudson <jchancehud@gmail.com>
2021-05-11 12:54:38 -0500

    refactor: remove key download logic

--- packages/cli/src/apps/wallet/config.ts
+++ packages/cli/src/apps/wallet/config.ts
@@ -4,9 +4,6 @@ export const DEFAULT = {
   chainId: 1,
   websocket: 'ws://goerli.zkopru.network:8546',
   coordinator: 'https://coordinator.zkopru.network',
-  // snarkkeys: 'https://zkopru.azureedge.net/snarkkeys/burrito/040946c7/keys.tgz',
-  snarkkeys:
-    'https://ipfs.tubby.cloud/ipfs/QmPAKEsw24rKjBzojMgyxHEnd9FXrW3Kgive3MfiYT3r79',
   maxBytes: 131072,
   priceMultiplier: 48,
   port: 8888,
