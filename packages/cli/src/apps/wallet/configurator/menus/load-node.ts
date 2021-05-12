1fee24c33c7cf394830ce3ee4dc6f6359023a1b2
Chance Hudson <jchancehud@gmail.com>
2021-05-12 01:53:14 -0500

    fix: use coordinator manager in wallet setup

--- packages/cli/src/apps/wallet/configurator/menus/load-node.ts
+++ packages/cli/src/apps/wallet/configurator/menus/load-node.ts
@@ -4,7 +4,9 @@ import {
   ZkopruNode,
   LightNode,
   HttpBootstrapHelper,
+  CoordinatorManager,
 } from '@zkopru/core'
+import Web3 from 'web3'
 import Configurator, { Context, Menu } from '../configurator'
 
 export default class LoadNode extends Configurator {
@@ -19,7 +21,7 @@ export default class LoadNode extends Configurator {
       throw Error('Websocket provider is not connected')
     let node: ZkopruNode
     const { provider, db, accounts } = context
-    const { address, coordinator: bootstrap } = this.base
+    const { address } = this.base
     if (this.base.fullnode) {
       node = await FullNode.new({
         provider,
@@ -28,14 +30,20 @@ export default class LoadNode extends Configurator {
         accounts,
       })
     } else {
+      const manager = new CoordinatorManager(address, new Web3(provider))
+      const coordinator = await manager.activeCoordinatorUrl()
+      if (!coordinator) {
+        this.print(`Unable to find coordinator to sync from`)
+        process.exit(1)
+      }
       node = await LightNode.new({
         provider,
         address,
         db,
         accounts,
-        bootstrapHelper: new HttpBootstrapHelper(bootstrap),
+        bootstrapHelper: new HttpBootstrapHelper(coordinator),
       })
-      this.print(chalk.blue(`Bootstrap light node from ${bootstrap}`))
+      this.print(chalk.blue(`Bootstrap light node from ${coordinator}`))
       await (node as LightNode).bootstrap()
     }
     return {
