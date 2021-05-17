f019866020adca4cadc894d4ff03a48a8cdb5e28
Jin <sifnoc09@gmail.com>
2021-05-18 00:32:48 +0900

    chore: removed comments and make up code

--- packages/generator/src/baseGenerator.ts
+++ packages/generator/src/baseGenerator.ts
@@ -1,19 +1,20 @@
 /* eslint-disable no-case-declarations */
+import fs from 'fs'
 import util from 'util'
 import Web3 from 'web3'
+import prettier from 'pino-pretty'
+import { Transform } from 'stream'
 import { networkInterfaces } from 'os'
 
 import { F } from '@zkopru/babyjubjub'
-import { Note } from '@zkopru/transaction'
-import { ZkAccount, HDWallet } from '@zkopru/account'
+import { Note, UtxoStatus } from '@zkopru/transaction'
+import { HDWallet, ZkAccount } from '@zkopru/account'
+import { logStream } from '@zkopru/utils'
 import { SQLiteConnector, schema } from '@zkopru/database/dist/node'
+import { ZkWallet } from '~zk-wizard/zk-wallet'
 
 // helper functions
-export async function getProviders(
-  url: string,
-  mnemonic: string,
-  password: string,
-) {
+export async function getBase(url: string, mnemonic: string, password: string) {
   const webSocketProvider = new Web3.providers.WebsocketProvider(url, {
     reconnect: { auto: true },
   })
@@ -30,23 +31,13 @@ export async function getProviders(
   await awaitConnection()
 
   // Create Wallet
-  const mockup = await SQLiteConnector.create(schema, ':memory:')
+  const mockupDB = await SQLiteConnector.create(schema, ':memory:')
   const web3 = new Web3(webSocketProvider)
-  const hdWallet = new HDWallet(web3, mockup)
+  const hdWallet = new HDWallet(web3, mockupDB)
 
   await hdWallet.init(mnemonic, password) //
 
-  return { hdWallet, webSocketProvider }
-}
-
-export async function genAccounts(hdWallet: HDWallet, num: number) {
-  const accounts: ZkAccount[] = []
-
-  for (let i = 0; i < num; i++) {
-    const account = await hdWallet.createAccount(i)
-    accounts.push(account)
-  }
-  return accounts
+  return { hdWallet, mockupDB, webSocketProvider }
 }
 
 export async function getDepositTx(wallet, note: Note, fee: F) {
@@ -83,6 +74,22 @@ export function logAll(Object) {
   })
 }
 
+export function startLogger(fileName: string) {
+  const writeStream = fs.createWriteStream(`/${fileName}`)
+  logStream.addStream(writeStream)
+  const pretty = prettier({
+    translateTime: false,
+    colorize: true,
+  })
+  const prettyStream = new Transform({
+    transform: (chunk, _, cb) => {
+      cb(null, pretty(JSON.parse(chunk.toString())))
+    },
+  })
+  prettyStream.pipe(process.stdout)
+  logStream.addStream(prettyStream)
+}
+
 // TODO: get fency current Coodinator Ip
 export function getLocalIP() {
   const nets = networkInterfaces()
@@ -95,4 +102,16 @@ export function getLocalIP() {
     throw new Error(`eth0 does not detected`)
   }
   return result
+}
+
+// TODO: create get only UserNote
+export async function getEthUtxo(wallet: ZkWallet, account: ZkAccount) {
+  const unSpentUtxo = await wallet.db.findMany('Utxo', {
+    where: {
+      owner: [account.zkAddress.toString()],
+      status: UtxoStatus.UNSPENT,
+      usedAt: null,
+    },
+  })
+  return unSpentUtxo
 }
