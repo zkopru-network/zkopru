78639abef40de5aebe9e1d098fc8cd498f3c9fdf
Chance Hudson <jchancehud@gmail.com>
2021-08-03 00:12:27 -0500

    chore: test light rollup tree against external implementation

---
+++ packages/tree/tests/unit/light-rollup-tree-test.ts
@@ -0,0 +1,122 @@
+import SparseTree from 'simple-smt'
+import { Fp } from '@zkopru/babyjubjub'
+import * as uuid from 'uuid'
+import assert from 'assert'
+import { LightRollUpTree } from '../../src/light-rollup-tree'
+import { genesisRoot, poseidonHasher } from '../../src/hasher'
+import sample from '~tree/sample'
+import { DB } from '~database'
+
+class TestTree extends LightRollUpTree<Fp> {
+  async indexesOfTrackingLeaves() {
+    return []
+  }
+}
+
+describe('rollup tree unit test', () => {
+  let testTree: TestTree
+  let mockup: DB
+  const depth = 20
+  beforeEach(async () => {
+    const { db } = await sample(depth)
+    mockup = db
+    const preHashes = poseidonHasher(depth).preHash
+    testTree = new TestTree({
+      db: mockup,
+      species: 10,
+      metadata: {
+        id: uuid.v4(),
+        species: 10,
+        start: Fp.from(0),
+        end: Fp.from(0),
+      },
+      data: {
+        root: genesisRoot(poseidonHasher(depth)),
+        index: Fp.zero,
+        siblings: preHashes.slice(0, -1),
+      },
+      config: {
+        hasher: poseidonHasher(depth),
+        forceUpdate: true,
+        fullSync: true,
+      },
+    })
+  })
+  afterEach(async () => {
+    await mockup.close()
+  })
+
+  describe('append()', () => {
+    it('should match external for single append', async () => {
+      const { parentOf, preHash } = poseidonHasher(depth)
+      const tree = new SparseTree<Fp>({
+        depth: depth + 1,
+        hashFn: (item1, item2) => {
+          const hash = parentOf(item1, item2)
+          return hash
+        },
+        preHashFn: (_, level) => {
+          return preHash[depth - level]
+        },
+        rightToLeft: true,
+      })
+      assert.equal(
+        tree.root().toString(),
+        testTree.root().toString(),
+        'Starting roots do not match',
+      )
+      for (let x = 1; x < 100; x++) {
+        await mockup.transaction(db => {
+          testTree.append(
+            [
+              {
+                hash: Fp.from(x),
+              },
+            ],
+            db,
+          )
+        })
+        tree.append(Fp.from(x))
+        assert.equal(
+          tree.root().toString(),
+          testTree.root().toString(),
+          `Roots do not match for index ${x}`,
+        )
+      }
+    })
+
+    it('should match external for batch append', async () => {
+      const { parentOf, preHash } = poseidonHasher(depth)
+      const tree = new SparseTree<Fp>({
+        depth: depth + 1,
+        hashFn: (item1, item2) => {
+          const hash = parentOf(item1, item2)
+          return hash
+        },
+        preHashFn: (_, level) => {
+          return preHash[depth - level]
+        },
+        rightToLeft: true,
+      })
+      assert.equal(
+        tree.root().toString(),
+        testTree.root().toString(),
+        'Starting roots do not match',
+      )
+      const leaves = Array(100)
+        .fill(null)
+        .map((_, index) => ({
+          hash: Fp.from(index),
+        }))
+      await mockup.transaction(db => {
+        testTree.append(leaves, db)
+      })
+      tree.appendMany(leaves.map(l => l.hash))
+      assert.equal(
+        tree.root().toString(),
+        testTree.root().toString(),
+        `Roots do not match`,
+      )
+    })
+  })
+})
