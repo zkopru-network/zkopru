92f21b58be3543f945b8133ecb668b10daf5a2ab
Chance Hudson <jchancehud@gmail.com>
2021-08-23 22:47:14 -0500

    feat: v2 memo withdraw sig abi

--- packages/transaction/src/memo.ts
+++ packages/transaction/src/memo.ts
@@ -16,6 +16,19 @@ export const V2_MEMO_DEFAULT_ABI = Bytes4.from(
   }),
 )
 
+export const V2_MEMO_WITHDRAW_SIG_ABI = Bytes4.from(
+  abiCoder.encodeFunctionSignature({
+    name: 'decodeWithdrawSigMemoV2',
+    type: 'function',
+    inputs: [
+      {
+        type: 'bytes[]',
+        name: 'memo',
+      },
+    ],
+  }),
+)
+
 export enum MemoVersion {
   V1 = 1,
   V2 = 2,
