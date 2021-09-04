50c03e631f1148abde81d1928606673fff88a8db
Wanseob Lim <email@wanseob.com>
2021-09-04 21:14:19 +0900

    fix: snark verify; resolves https://github.com/zkopru-network/zkopru/pull/257\#discussion_r698118726

--- packages/core/src/snark/snark-child-process.ts
+++ packages/core/src/snark/snark-child-process.ts
@@ -8,12 +8,11 @@ process.on(
     const { vk, proof, signals } = message
     let result!: boolean
     try {
-      snarkjs.groth16.verify(
+      result = await snarkjs.groth16.verify(
         ffjs.utils.unstringifyBigInts(vk),
         ffjs.utils.unstringifyBigInts(signals),
         ffjs.utils.unstringifyBigInts(proof),
       )
-      result = true
     } catch (e) {
       logger.error(e)
       result = false
