17d13855986c5866ab950a61ba97abed5a6f7791
sifnoc <sifnoc09@gmail.com>
2022-03-22 17:22:53 +0000

    fix: limit massDeposits for including propose tx

--- packages/core/src/context/layer2.ts
+++ packages/core/src/context/layer2.ts
@@ -265,6 +265,8 @@ export class L2Chain {
     // 1. pick mass deposits
     const commits: MassDepositSql[] = await this.db.findMany('MassDeposit', {
       where: { includedIn: null },
+      orderBy: { blockNumber: 'asc' },
+      limit: 255
     })
     commits.sort((a, b) => parseInt(a.index, 10) - parseInt(b.index, 10))
     const pendingDeposits = await this.db.findMany('Deposit', {
