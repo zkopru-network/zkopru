d6c790f926f3da1a3cfd584dd6f2af5b7c22b27d
Wanseob Lim <email@wanseob.com>
2022-04-06 20:48:19 +0900

    chore: lint

--- packages/circuits/tests/spending_pubkey.test.ts
+++ packages/circuits/tests/spending_pubkey.test.ts
@@ -7,6 +7,7 @@
 
 import fs from 'fs'
 import { genSNARK, SNARKResult } from '~zk-wizard/snark'
+import { accounts } from '~dataset/testset-predefined'
 import {
   checkPhase1Setup,
   compileCircuit,
@@ -14,7 +15,6 @@ import {
   phase2Setup,
   prepareArtifactsDirectory,
 } from './helper'
-import { accounts } from '~dataset/testset-predefined'
 
 const fileName = 'spending_pubkey.test.circom'
 const artifacts = getArtifacts(fileName)
