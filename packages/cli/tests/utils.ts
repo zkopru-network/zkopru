62af162eb144eb17fa2ca9ad8d742d0c3386c26c
KimiWu <leonhartwu@gmail.com>
2022-09-06 11:17:22 +0800

    test(cli): refactor mock objects

--- packages/cli/tests/utils.ts
+++ packages/cli/tests/utils.ts
@@ -1,46 +1,6 @@
+/* eslint-disable no-case-declarations */
 import fs from 'fs'
-import { PromptApp } from '@zkopru/utils'
-import LoadDatabase from '../src/apps/wallet/configurator/menus/load-database'
-import LoadHDWallet from '../src/apps/wallet/configurator/menus/load-hdwallet'
-import TrackingAccount from '../src/apps/wallet/configurator/menus/config-tracking-accounts'
-import SaveConfig from '../src/apps/wallet/configurator/menus/save-config'
-import { Config, Context } from '../src/apps/wallet/configurator/configurator'
-
-export function mockedLoadDatabase(
-  option: any,
-): jest.Mocked<PromptApp<Context, Config>> {
-  const obj = new LoadDatabase(option)
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, Config>>
-  mockedObj.ask = jest.fn()
-  return mockedObj
-}
-
-export function mockedLoadHDWallet(
-  option: any,
-): jest.Mocked<PromptApp<Context, Config>> {
-  const obj = new LoadHDWallet(option)
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, Config>>
-  mockedObj.ask = jest.fn()
-  return mockedObj
-}
-
-export function mockedTrackingAccount(
-  option: any,
-): jest.Mocked<PromptApp<Context, Config>> {
-  const obj = new TrackingAccount(option)
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, Config>>
-  mockedObj.ask = jest.fn()
-  return mockedObj
-}
-
-export function mockedSaveConfig(
-  option: any,
-): jest.Mocked<PromptApp<Context, Config>> {
-  const obj = new SaveConfig(option)
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, Config>>
-  mockedObj.ask = jest.fn()
-  return mockedObj
-}
+import { Config } from '../src/apps/wallet/configurator/configurator'
 
 export function loadConfig(path: string): Config {
   const config = {
