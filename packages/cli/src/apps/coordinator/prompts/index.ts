12ab6bc8c7a34eae5f5a8193aebb4213be0047f6
KimiWu <leonhartwu@gmail.com>
2022-09-25 17:43:35 +0800

    test(cli): test cases for cli/coordinator/prompts were done

--- packages/cli/src/apps/coordinator/prompts/index.ts
+++ packages/cli/src/apps/coordinator/prompts/index.ts
@@ -1,6 +1,6 @@
 import { PromptApp } from '@zkopru/utils'
 import { Coordinator } from '@zkopru/coordinator'
-import { Dashboard } from '../../../dashboard'
+// import { Dashboard } from '../../../dashboard'
 
 export enum AppMenu {
   PRINT_STATUS,
@@ -19,8 +19,8 @@ export enum AppMenu {
   REGISTER_AS_COORDINATOR,
   DEREGISTER,
   COMMIT_DEPOSITS,
-  TOP_MENU = Dashboard.START_CODE,
-  EXIT = Dashboard.EXIT_CODE,
+  TOP_MENU = -777, //Dashboard.START_CODE,
+  EXIT = -999, //Dashboard.EXIT_CODE,
 }
 
 export interface Context {
