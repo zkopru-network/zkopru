bba266e3a32546f40908abeab0830902a4b02229
sifnoc <sifnoc09@gmail.com>
2022-10-31 13:35:29 +0000

    fix: using defualt port

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -16,7 +16,7 @@ const config: HardhatUserConfig = {
   solidity: "0.7.4",
   networks: {
     testnet: {
-      url: "http://localhost:8546",
+      url: process.env.HARDHAT_URL || "http://localhost:8545",
     },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
