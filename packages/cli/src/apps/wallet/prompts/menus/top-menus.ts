0c3576e617f19b7636ff759c1514a53e739e9819
KimiWu <leonhartwu@gmail.com>
2022-11-01 09:46:34 +0800

    feat(cli): add a L2 account which is the same as the one generated by MetaMask in wallet menu

--- packages/cli/src/apps/wallet/prompts/menus/top-menus.ts
+++ packages/cli/src/apps/wallet/prompts/menus/top-menus.ts
@@ -12,9 +12,10 @@ export default class TopMenu extends App {
       name: 'idx',
       message: 'Which account do you want to use?',
       choices: [
+        { title: `${accounts[0].ethAddress} - metamask`, value: 0 },
         ...accounts.map((obj, i) => ({
-          title: obj.ethAddress,
-          value: i,
+          title: `${obj.ethAddress} - keystore`,
+          value: i + 10,
         })),
         {
           title: 'Create new account',
@@ -28,6 +29,13 @@ export default class TopMenu extends App {
     })
     let reRun: { context: Context; next: number }
     switch (idx) {
+      case 0:
+        const accountMetaMask = await this.getZkAccountBySignature()
+        this.base.setAccount(accountMetaMask)
+        return {
+          context: { ...context, account: accountMetaMask },
+          next: AppMenu.ACCOUNT_DETAIL,
+        }
       case -1:
         await this.base.createAccount(accounts.length)
         reRun = await this.run(context)
@@ -35,11 +43,38 @@ export default class TopMenu extends App {
       case -2:
         return { context, next: AppMenu.EXIT }
       default:
-        this.base.setAccount(accounts[idx])
+        this.base.setAccount(accounts[idx - 10])
         return {
-          context: { ...context, account: accounts[idx] },
+          context: { ...context, account: accounts[idx - 10] },
           next: AppMenu.ACCOUNT_DETAIL,
         }
     }
+  }
+
+  async getZkAccountBySignature(): Promise<ZkAccount> {
+    const chainId = (await this.base.node.layer1.provider.getNetwork()).chainId
+    const domain = {
+      chainId: chainId,
+      name: 'Zkopru Testnet',
+      version: '0',
+    }
+    const message = {
+      info: 'Unlock Zkopru wallet',
+      warning:
+        'This signature is your private key, only sign on official Zkopru websites!',
+    }
+    const types = {
+      ZkopruKey: [
+        { name: 'info', type: 'string' },
+        { name: 'warning', type: 'string' },
+      ],
+    }
+    const signedData = await this.base.accounts[0].ethAccount._signTypedData(
+      domain,
+      types,
+      message,
+    )
+    const { sha512_256 } = await import(/* webpackPrefetch: true */ 'js-sha512')
+    return new ZkAccount(sha512_256(signedData), this.base.node.layer1.provider)
   }
 }
