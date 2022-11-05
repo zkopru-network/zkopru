635693fd4c1e5645e4ceebbf1a152dcaf4b6449c
KimiWu <leonhartwu@gmail.com>
2022-11-05 13:43:18 +0800

    feat: store l2 private key instead of l1 private key in ZkAccount

--- packages/cli/tests/unit/wallet/mocksForPrompts.ts
+++ packages/cli/tests/unit/wallet/mocksForPrompts.ts
@@ -1,134 +1,134 @@
 /* eslint-disable no-case-declarations */
 
-import { PromptApp } from "@zkopru/utils";
-import { ZkWallet } from "@zkopru/zk-wizard";
-import { Context } from "../../../src/apps/wallet/prompts/index";
-import DepositEther from "../../../src/apps/wallet/prompts/menus/account-detail-deposit-eth";
-import Deposit from "../../../src/apps/wallet/prompts/menus/account-detail-deposit";
-import TopMenu from "../../../src/apps/wallet/prompts/menus/top-menus";
-import AccountDetail from "../../../src/apps/wallet/prompts/menus/account-detail";
-import WithdrawableList from "../../../src/apps/wallet/prompts/menus/account-detail-withdrawable-list";
-import Withdraw from "../../../src/apps/wallet/prompts/menus/account-detail-withdraw";
-import WithdrawRequest from "../../../src/apps/wallet/prompts/menus/account-detail-withdraw-request-menu";
-import WithdrawRequestEth from "../../../src/apps/wallet/prompts/menus/account-detail-withdraw-request-eth";
-import TransferMenu from "../../../src/apps/wallet/prompts/menus/account-detail-transfer-menu";
-import TransferEth from "../../../src/apps/wallet/prompts/menus/account-detail-transfer-eth";
-import AtomicSwap from "../../../src/apps/wallet/prompts/menus/account-detail-swap";
-import AtomicSwapTake from "../../../src/apps/wallet/prompts/menus/account-detail-swap-take";
-import AtomicSwapTakeEth from "../../../src/apps/wallet/prompts/menus/account-detail-swap-take-eth";
-import AtomicSwapGiveEth from "../../../src/apps/wallet/prompts/menus/account-detail-swap-give-eth";
+import { PromptApp } from '@zkopru/utils'
+import { ZkWallet } from '@zkopru/zk-wizard'
+import { Context } from '../../../src/apps/wallet/prompts/index'
+import DepositEther from '../../../src/apps/wallet/prompts/menus/account-detail-deposit-eth'
+import Deposit from '../../../src/apps/wallet/prompts/menus/account-detail-deposit'
+import TopMenu from '../../../src/apps/wallet/prompts/menus/top-menus'
+import AccountDetail from '../../../src/apps/wallet/prompts/menus/account-detail'
+import WithdrawableList from '../../../src/apps/wallet/prompts/menus/account-detail-withdrawable-list'
+import Withdraw from '../../../src/apps/wallet/prompts/menus/account-detail-withdraw'
+import WithdrawRequest from '../../../src/apps/wallet/prompts/menus/account-detail-withdraw-request-menu'
+import WithdrawRequestEth from '../../../src/apps/wallet/prompts/menus/account-detail-withdraw-request-eth'
+import TransferMenu from '../../../src/apps/wallet/prompts/menus/account-detail-transfer-menu'
+import TransferEth from '../../../src/apps/wallet/prompts/menus/account-detail-transfer-eth'
+import AtomicSwap from '../../../src/apps/wallet/prompts/menus/account-detail-swap'
+import AtomicSwapTake from '../../../src/apps/wallet/prompts/menus/account-detail-swap-take'
+import AtomicSwapTakeEth from '../../../src/apps/wallet/prompts/menus/account-detail-swap-take-eth'
+import AtomicSwapGiveEth from '../../../src/apps/wallet/prompts/menus/account-detail-swap-give-eth'
 
 export function mockDepositEther(
-  option: any
+  option: any,
 ): jest.Mocked<PromptApp<Context, ZkWallet>> {
-  const obj = new DepositEther(option);
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>;
-  mockedObj.ask = jest.fn();
-  return mockedObj;
+  const obj = new DepositEther(option)
+  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>
+  mockedObj.ask = jest.fn()
+  return mockedObj
 }
 
 export function mockDeposit(
-  option: any
+  option: any,
 ): jest.Mocked<PromptApp<Context, ZkWallet>> {
-  const obj = new Deposit(option);
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>;
-  mockedObj.ask = jest.fn();
-  return mockedObj;
+  const obj = new Deposit(option)
+  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>
+  mockedObj.ask = jest.fn()
+  return mockedObj
 }
 
 export function mockAtomicSwapGiveEth(
-  option: any
+  option: any,
 ): jest.Mocked<PromptApp<Context, ZkWallet>> {
-  const obj = new AtomicSwapGiveEth(option);
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>;
-  mockedObj.ask = jest.fn();
-  return mockedObj;
+  const obj = new AtomicSwapGiveEth(option)
+  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>
+  mockedObj.ask = jest.fn()
+  return mockedObj
 }
 export function mockAtomicSwapTakeEth(
-  option: any
+  option: any,
 ): jest.Mocked<PromptApp<Context, ZkWallet>> {
-  const obj = new AtomicSwapTakeEth(option);
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>;
-  mockedObj.ask = jest.fn();
-  return mockedObj;
+  const obj = new AtomicSwapTakeEth(option)
+  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>
+  mockedObj.ask = jest.fn()
+  return mockedObj
 }
 export function mockAtomicSwapTake(
-  option: any
+  option: any,
 ): jest.Mocked<PromptApp<Context, ZkWallet>> {
-  const obj = new AtomicSwapTake(option);
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>;
-  mockedObj.ask = jest.fn();
-  return mockedObj;
+  const obj = new AtomicSwapTake(option)
+  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>
+  mockedObj.ask = jest.fn()
+  return mockedObj
 }
 export function mockAtomicSwap(
-  option: any
+  option: any,
 ): jest.Mocked<PromptApp<Context, ZkWallet>> {
-  const obj = new AtomicSwap(option);
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>;
-  mockedObj.ask = jest.fn();
-  return mockedObj;
+  const obj = new AtomicSwap(option)
+  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>
+  mockedObj.ask = jest.fn()
+  return mockedObj
 }
 export function mockTransferEth(
-  option: any
+  option: any,
 ): jest.Mocked<PromptApp<Context, ZkWallet>> {
-  const obj = new TransferEth(option);
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>;
-  mockedObj.ask = jest.fn();
-  return mockedObj;
+  const obj = new TransferEth(option)
+  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>
+  mockedObj.ask = jest.fn()
+  return mockedObj
 }
 export function mockTransferMenu(
-  option: any
+  option: any,
 ): jest.Mocked<PromptApp<Context, ZkWallet>> {
-  const obj = new TransferMenu(option);
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>;
-  mockedObj.ask = jest.fn();
-  return mockedObj;
+  const obj = new TransferMenu(option)
+  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>
+  mockedObj.ask = jest.fn()
+  return mockedObj
 }
 export function mockWithdrawRequestEth(
-  option: any
+  option: any,
 ): jest.Mocked<PromptApp<Context, ZkWallet>> {
-  const obj = new WithdrawRequestEth(option);
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>;
-  mockedObj.ask = jest.fn();
-  return mockedObj;
+  const obj = new WithdrawRequestEth(option)
+  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>
+  mockedObj.ask = jest.fn()
+  return mockedObj
 }
 export function mockWithdrawRequest(
-  option: any
+  option: any,
 ): jest.Mocked<PromptApp<Context, ZkWallet>> {
-  const obj = new WithdrawRequest(option);
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>;
-  mockedObj.ask = jest.fn();
-  return mockedObj;
+  const obj = new WithdrawRequest(option)
+  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>
+  mockedObj.ask = jest.fn()
+  return mockedObj
 }
 export function mockWithdraw(
-  option: any
+  option: any,
 ): jest.Mocked<PromptApp<Context, ZkWallet>> {
-  const obj = new Withdraw(option);
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>;
-  mockedObj.ask = jest.fn();
-  return mockedObj;
+  const obj = new Withdraw(option)
+  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>
+  mockedObj.ask = jest.fn()
+  return mockedObj
 }
 export function mockWithdrawableList(
-  option: any
+  option: any,
 ): jest.Mocked<PromptApp<Context, ZkWallet>> {
-  const obj = new WithdrawableList(option);
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>;
-  mockedObj.ask = jest.fn();
-  return mockedObj;
+  const obj = new WithdrawableList(option)
+  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>
+  mockedObj.ask = jest.fn()
+  return mockedObj
 }
 export function mockAccountDetail(
-  option: any
+  option: any,
 ): jest.Mocked<PromptApp<Context, ZkWallet>> {
-  const obj = new AccountDetail(option);
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>;
-  mockedObj.ask = jest.fn();
-  return mockedObj;
+  const obj = new AccountDetail(option)
+  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>
+  mockedObj.ask = jest.fn()
+  return mockedObj
 }
 export function mockTopMenu(
-  option: any
+  option: any,
 ): jest.Mocked<PromptApp<Context, ZkWallet>> {
-  const obj = new TopMenu(option);
-  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>;
-  mockedObj.ask = jest.fn();
-  return mockedObj;
+  const obj = new TopMenu(option)
+  const mockedObj = obj as jest.Mocked<PromptApp<Context, ZkWallet>>
+  mockedObj.ask = jest.fn()
+  return mockedObj
 }
