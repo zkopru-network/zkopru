401442effee0ab0045993725e9ca7f299ea5fdb9
sifnoc <sifnoc09@gmail.com>
2023-01-07 16:38:39 +0900

    fix: type in wsprovider

--- packages/cli/src/apps/coordinator/configurator/config-prompts/connect-web3.ts
+++ packages/cli/src/apps/coordinator/configurator/config-prompts/connect-web3.ts
@@ -4,78 +4,87 @@ import Configurator, { Context, Menu } from '../configurator'
 
 // this provider code block comes in here
 // https://github.com/ethers-io/ethers.js/issues/1053
-const WEBSOCKET_PING_INTERVAL = 10000;
-const WEBSOCKET_PONG_TIMEOUT = 5000;
-const WEBSOCKET_RECONNECT_DELAY = 100;
+const WEBSOCKET_PING_INTERVAL = 10000
+const WEBSOCKET_PONG_TIMEOUT = 5000
+const WEBSOCKET_RECONNECT_DELAY = 100
 
-const WebSocketProviderClass = (): new () => ethers.providers.WebSocketProvider => (class { } as never);
+const WebSocketProviderClass = (): new () => ethers.providers.WebSocketProvider =>
+  class {} as never
 
 export class WebSocketProvider extends WebSocketProviderClass() {
-  private provider?: ethers.providers.WebSocketProvider;
-  private events: ethers.providers.WebSocketProvider['_events'] = [];
-  private requests: ethers.providers.WebSocketProvider['_requests'] = {};
+  private provider?: ethers.providers.WebSocketProvider
+
+  private events: ethers.providers.WebSocketProvider['_events'] = []
+
+  private requests: ethers.providers.WebSocketProvider['_requests'] = {}
 
   private handler = {
     get(target: WebSocketProvider, prop: string, receiver: unknown) {
-      const value = target.provider && Reflect.get(target.provider, prop, receiver);
+      const value =
+        target.provider && Reflect.get(target.provider, prop, receiver)
 
-      return value instanceof Function ? value.bind(target.provider) : value;
+      return value instanceof Function ? value.bind(target.provider) : value
     },
-  };
+  }
 
   constructor(private providerUrl) {
-    super();
-    this.create();
+    super()
+    this.create()
 
-    return new Proxy(this, this.handler);
+    return new Proxy(this, this.handler)
   }
 
   private create() {
     if (this.provider) {
-      this.events = [...this.events, ...this.provider._events];
-      this.requests = { ...this.requests, ...this.provider._requests };
+      this.events = [...this.events, ...this.provider._events]
+      this.requests = { ...this.requests, ...this.provider._requests }
     }
 
-    const provider = new ethers.providers.WebSocketProvider(this.providerUrl, this.provider?.network?.chainId);
-    let pingInterval: NodeJS.Timer | undefined;
-    let pongTimeout: NodeJS.Timeout | undefined;
+    const provider = new ethers.providers.WebSocketProvider(
+      this.providerUrl,
+      this.provider?.network?.chainId,
+    )
+    let pingInterval: any // NodeJS.Timer
+    let pongTimeout: any // NodeJS.Timeout
 
     provider._websocket.on('open', () => {
       pingInterval = setInterval(() => {
-        provider._websocket.ping();
+        provider._websocket.ping()
 
-        pongTimeout = setTimeout(() => { provider._websocket.terminate(); }, WEBSOCKET_PONG_TIMEOUT);
-      }, WEBSOCKET_PING_INTERVAL);
+        pongTimeout = setTimeout(() => {
+          provider._websocket.terminate()
+        }, WEBSOCKET_PONG_TIMEOUT)
+      }, WEBSOCKET_PING_INTERVAL)
 
-      let event;
+      let event
       while ((event = this.events.pop())) {
-        provider._events.push(event);
-        provider._startEvent(event);
+        provider._events.push(event)
+        provider._startEvent(event)
       }
 
       for (const key in this.requests) {
-        provider._requests[key] = this.requests[key];
-        provider._websocket.send(this.requests[key].payload);
-        delete this.requests[key];
+        provider._requests[key] = this.requests[key]
+        provider._websocket.send(this.requests[key].payload)
+        delete this.requests[key]
       }
-    });
+    })
 
     provider._websocket.on('pong', () => {
-      if (pongTimeout) clearTimeout(pongTimeout);
-    });
+      if (pongTimeout) clearTimeout(pongTimeout)
+    })
 
     provider._websocket.on('close', (code: number) => {
-      provider._wsReady = false;
+      provider._wsReady = false
 
-      if (pingInterval) clearInterval(pingInterval);
-      if (pongTimeout) clearTimeout(pongTimeout);
+      if (pingInterval) clearInterval(pingInterval)
+      if (pongTimeout) clearTimeout(pongTimeout)
 
       if (code !== 1000) {
-        setTimeout(() => this.create(), WEBSOCKET_RECONNECT_DELAY);
+        setTimeout(() => this.create(), WEBSOCKET_RECONNECT_DELAY)
       }
-    });
+    })
 
-    this.provider = provider;
+    this.provider = provider
   }
 }
 
@@ -85,7 +94,7 @@ export default class ConnectWeb3 extends Configurator {
   async run(context: Context): Promise<{ context: Context; next: number }> {
     console.log(chalk.blue('Connecting to the Ethereum network'))
 
-    const provider = new WebSocketProvider(this.base.provider);
+    const provider = new WebSocketProvider(this.base.provider)
 
     console.log(chalk.blue(`Connected via ${this.base.provider}`))
     return {
